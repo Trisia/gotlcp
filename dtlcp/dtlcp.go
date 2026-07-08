8de9ab502ecf8081d214240965a2abe68f0c9bf9
19011 <quanguanyu@qq.com>
2026-07-08 09:33:58 +0800

    入口

---
+++ dtlcp/dtlcp.go
@@ -0,0 +1,295 @@
+// Copyright (c) 2025 gotlcp contributors
+// gotlcp is licensed under Mulan PSL v2.
+
+// DTLCP 入口点：Server、Client、Dial、Listen
+
+package dtlcp
+
+import (
+	"context"
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/rsa"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/emmansun/gmsm/sm2"
+	x509 "github.com/emmansun/gmsm/smx509"
+)
+
+// Server 使用现有连接对象构造一个新的 DTLCP 服务端连接对象。
+// 配置参数对象 config 不能为空，且至少提供签名密钥对和加密密钥以及签名证书和加密证书。
+func Server(pconn net.PacketConn, addr net.Addr, config *Config) *Conn {
+	c := &Conn{
+		pconn:            pconn,
+		remoteAddr:       addr,
+		config:           config,
+		isClient:         false,
+		hsState:          statePreparing,
+		messageSeq:       0,
+		nextReceiveSeq:   0,
+		writeEpoch:       0,
+		readEpoch:        0,
+		writeSeq:         0,
+		readSeq:          0,
+		pendingFragments: make(map[uint16]*fragmentBuffer),
+	}
+	c.handshakeFn = c.serverHandshake
+	c.initRetransmitTimer(config)
+	return c
+}
+
+// Client 使用现有连接对象构造一个新的 DTLCP 客户端连接对象。
+func Client(pconn net.PacketConn, addr net.Addr, config *Config) *Conn {
+	c := &Conn{
+		pconn:            pconn,
+		remoteAddr:       addr,
+		config:           config,
+		isClient:         true,
+		hsState:          statePreparing,
+		messageSeq:       0,
+		nextReceiveSeq:   0,
+		writeEpoch:       0,
+		readEpoch:        0,
+		writeSeq:         0,
+		readSeq:          0,
+		pendingFragments: make(map[uint16]*fragmentBuffer),
+	}
+	c.handshakeFn = c.clientHandshake
+	c.initRetransmitTimer(config)
+	return c
+}
+
+// initRetransmitTimer 初始化重传定时器
+func (c *Conn) initRetransmitTimer(config *Config) {
+	initialTimeout := config.InitialRetransmitTimeout
+	if initialTimeout <= 0 {
+		initialTimeout = defaultInitialRetransmitTimeout
+	}
+	maxTimeout := config.MaxRetransmitTimeout
+	if maxTimeout <= 0 {
+		maxTimeout = defaultMaxRetransmitTimeout
+	}
+	newTimer := config.NewTimer
+	if newTimer == nil {
+		newTimer = defaultNewTimer
+	}
+	c.retransmitTimer = newRetransmitTimer(initialTimeout, maxTimeout, newTimer)
+}
+
+const (
+	defaultInitialRetransmitTimeout = 1 * time.Second
+	defaultMaxRetransmitTimeout     = 64 * time.Second
+)
+
+// listener 实现了 net.Listener 接口，用于表示 DTLCP 的 Listener。
+// 注意：DTLCP 基于 PacketConn，标准 listener 模式不直接适用，这里提供兼容接口供测试使用。
+type listener struct {
+	net.Listener
+	config *Config
+}
+
+// Accept 等待并返还一个 DTLCP 连接对象。
+func (l *listener) Accept() (net.Conn, error) {
+	conn, err := l.Listener.Accept()
+	if err != nil {
+		return nil, err
+	}
+	return Server(conn.(net.PacketConn), conn.RemoteAddr(), l.config), nil
+}
+
+// NewListener 基于现有的 net.Listener 创建 DTLCP Listener 对象。
+func NewListener(inner net.Listener, config *Config) net.Listener {
+	return &listener{Listener: inner, config: config}
+}
+
+// Listen 在指定的网络协议上，监听指定地址的端口，创建一个 DTLCP Listener。
+func Listen(network, laddr string, config *Config) (net.Listener, error) {
+	if config == nil || len(config.Certificates) == 0 &&
+		config.GetCertificate == nil && config.GetConfigForClient == nil {
+		return nil, errors.New("dtlcp: neither Certificates, GetCertificate, nor GetConfigForClient set in Config")
+	}
+	l, err := net.Listen(network, laddr)
+	if err != nil {
+		return nil, err
+	}
+	return NewListener(l, config), nil
+}
+
+// Dial 建立 DTLCP 客户端连接
+func Dial(network, addr string, config *Config) (*Conn, error) {
+	return DialContext(context.Background(), network, addr, config)
+}
+
+// DialContext 在给定上下文中建立 DTLCP 客户端连接
+func DialContext(ctx context.Context, network, addr string, config *Config) (*Conn, error) {
+	rawConn, err := net.Dial(network, addr)
+	if err != nil {
+		return nil, err
+	}
+	udpConn, ok := rawConn.(net.PacketConn)
+	if !ok {
+		rawConn.Close()
+		return nil, errors.New("dtlcp: dialed connection does not implement PacketConn")
+	}
+	remoteAddr := rawConn.RemoteAddr()
+
+	if config == nil {
+		config = defaultConfig()
+	}
+
+	conn := Client(udpConn, remoteAddr, config)
+	if err := conn.HandshakeContext(ctx); err != nil {
+		rawConn.Close()
+		return nil, err
+	}
+	return conn, nil
+}
+
+// Dialer DTLCP 客户端拨号器
+type Dialer struct {
+	// NetDialer 底层网络拨号器，默认使用 net.Dialer
+	NetDialer *net.Dialer
+	// Config DTLCP 配置
+	Config *Config
+}
+
+func (d *Dialer) netDialer() *net.Dialer {
+	if d.NetDialer != nil {
+		return d.NetDialer
+	}
+	return new(net.Dialer)
+}
+
+// Dial 建立 DTLCP 连接。
+func (d *Dialer) Dial(network, addr string) (net.Conn, error) {
+	return d.DialContext(context.Background(), network, addr)
+}
+
+// DialContext 在给定上下文中建立 DTLCP 连接。
+func (d *Dialer) DialContext(ctx context.Context, network, addr string) (net.Conn, error) {
+	return DialContext(ctx, network, addr, d.Config)
+}
+
+// LoadX509KeyPair 从文件读取证书和密钥对。
+func LoadX509KeyPair(certFile, keyFile string) (Certificate, error) {
+	certPEMBlock, err := os.ReadFile(certFile)
+	if err != nil {
+		return Certificate{}, err
+	}
+	keyPEMBlock, err := os.ReadFile(keyFile)
+	if err != nil {
+		return Certificate{}, err
+	}
+	return X509KeyPair(certPEMBlock, keyPEMBlock)
+}
+
+// X509KeyPair 解析 PEM 编码的数字证书和私钥。
+func X509KeyPair(certPEMBlock, keyPEMBlock []byte) (Certificate, error) {
+	fail := func(err error) (Certificate, error) { return Certificate{}, err }
+
+	var cert Certificate
+	var skippedBlockTypes []string
+	for {
+		var certDERBlock *pem.Block
+		certDERBlock, certPEMBlock = pem.Decode(certPEMBlock)
+		if certDERBlock == nil {
+			break
+		}
+		if certDERBlock.Type == "CERTIFICATE" {
+			cert.Certificate = append(cert.Certificate, certDERBlock.Bytes)
+		} else {
+			skippedBlockTypes = append(skippedBlockTypes, certDERBlock.Type)
+		}
+	}
+
+	if len(cert.Certificate) == 0 {
+		if len(skippedBlockTypes) == 0 {
+			return fail(errors.New("dtlcp: failed to find any PEM data in certificate input"))
+		}
+		if len(skippedBlockTypes) == 1 && strings.HasSuffix(skippedBlockTypes[0], "PRIVATE KEY") {
+			return fail(errors.New("dtlcp: failed to find certificate PEM data in certificate input, but did find a private key; PEM inputs may have been switched"))
+		}
+		return fail(fmt.Errorf("dtlcp: failed to find \"CERTIFICATE\" PEM block in certificate input after skipping PEM blocks of the following types: %v", skippedBlockTypes))
+	}
+
+	skippedBlockTypes = skippedBlockTypes[:0]
+	var keyDERBlock *pem.Block
+	for {
+		keyDERBlock, keyPEMBlock = pem.Decode(keyPEMBlock)
+		if keyDERBlock == nil {
+			if len(skippedBlockTypes) == 0 {
+				return fail(errors.New("dtlcp: failed to find any PEM data in key input"))
+			}
+			if len(skippedBlockTypes) == 1 && skippedBlockTypes[0] == "CERTIFICATE" {
+				return fail(errors.New("dtlcp: found a certificate rather than a key in the PEM for the private key"))
+			}
+			return fail(fmt.Errorf("dtlcp: failed to find PEM block with type ending in \"PRIVATE KEY\" in key input after skipping PEM blocks of the following types: %v", skippedBlockTypes))
+		}
+		if keyDERBlock.Type == "PRIVATE KEY" || strings.HasSuffix(keyDERBlock.Type, " PRIVATE KEY") {
+			break
+		}
+		skippedBlockTypes = append(skippedBlockTypes, keyDERBlock.Type)
+	}
+
+	x509Cert, err := x509.ParseCertificate(cert.Certificate[0])
+	if err != nil {
+		return fail(err)
+	}
+	cert.Leaf = x509Cert
+
+	cert.PrivateKey, err = parsePrivateKey(keyDERBlock.Bytes)
+	if err != nil {
+		return fail(err)
+	}
+	switch pub := x509Cert.PublicKey.(type) {
+	case *rsa.PublicKey:
+		priv, ok := cert.PrivateKey.(*rsa.PrivateKey)
+		if !ok {
+			return fail(errors.New("dtlcp: private key type does not match public key type"))
+		}
+		if pub.N.Cmp(priv.N) != 0 {
+			return fail(errors.New("dtlcp: private key does not match public key"))
+		}
+	case *ecdsa.PublicKey:
+		priv, ok := cert.PrivateKey.(*sm2.PrivateKey)
+		if !ok {
+			return fail(errors.New("dtlcp: private key type does not match public key type"))
+		}
+		if pub.X.Cmp(priv.X) != 0 || pub.Y.Cmp(priv.Y) != 0 {
+			return fail(errors.New("dtlcp: private key does not match public key"))
+		}
+	default:
+		return fail(errors.New("dtlcp: unknown public key algorithm"))
+	}
+
+	return cert, nil
+}
+
+// parsePrivateKey 解析 PKCS8 格式 SM2 密钥对。
+func parsePrivateKey(der []byte) (crypto.PrivateKey, error) {
+	if key, err := x509.ParsePKCS8PrivateKey(der); err == nil {
+		switch key := key.(type) {
+		case *rsa.PrivateKey, *sm2.PrivateKey:
+			return key, nil
+		case *ecdsa.PrivateKey:
+			return nil, errors.New("dtlcp: non-SM2 curve in PKCS#8 private key")
+		default:
+			return nil, errors.New("dtlcp: found unknown private key type in PKCS#8 wrapping")
+		}
+	}
+	if key, err := x509.ParseTypedECPrivateKey(der); err == nil {
+		switch key := key.(type) {
+		case *sm2.PrivateKey:
+			return key, nil
+		default:
+			return nil, errors.New("dtlcp: non-SM2 curve in EC private key")
+		}
+	}
+	return nil, errors.New("dtlcp: failed to parse SM2/RSA private key")
+}
